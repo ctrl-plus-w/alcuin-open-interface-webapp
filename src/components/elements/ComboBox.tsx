7b8e0f67c1c1c576fe341b0ec7fb6cc11ec1b605
Lukas Laudrain <lukas.ldrn@gmail.com>
2023-10-23 11:27:22 +0200

    :bug: Fixed some linting

--- src/components/elements/ComboBox.tsx
+++ src/components/elements/ComboBox.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
-import { Check, ChevronsUpDown } from 'lucide-react';
-
 import { Button } from '@/components/ui/button';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Check, ChevronsUpDown } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
