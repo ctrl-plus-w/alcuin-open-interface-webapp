2e3f515c21afdc3ad5e9048d3709e8a97f95ccb9
Lukas Laudrain <lukas.ldrn@gmail.com>
2023-12-13 16:14:02 +0100

    :bug: Reverting bug tests

--- src/components/features/grades/SettingsForm.tsx
+++ src/components/features/grades/SettingsForm.tsx
@@ -14,7 +14,7 @@ import { useToast } from '@/ui/use-toast';
 import useProfilesRepository from '@/hook/useProfilesRepository';
 
 import { onChange } from '@/util/react.util';
-// import { encryptDataWithRSA } from '@/util/string.util';
+import { encryptDataWithRSA } from '@/util/string.util';
 import { cn } from '@/util/style.util';
 
 export enum State {
@@ -49,9 +49,6 @@ const SettingsForm = ({ state, user, setUser, className }: IProps): ReactElement
     try {
       const rsaPublicKeyStr = process.env.NEXT_PUBLIC_RSA_PUBLIC_KEY;
 
-      // eslint-disable-next-line no-console
-      console.log(rsaPublicKeyStr);
-
       if (!rsaPublicKeyStr) {
         console.error('Missing environment variable `NEXT_PUBLIC_RSA_PUBLIC_KEY`.');
         throw new Error('Invalid server config, please contact the administrator.');
@@ -59,12 +56,9 @@ const SettingsForm = ({ state, user, setUser, className }: IProps): ReactElement
 
       const rsaPublicKey = rsaPublicKeyStr.split('\n').join('\n');
 
-      // eslint-disable-next-line no-console
-      console.log(rsaPublicKey);
-
-      // const encryptedPassword = encryptDataWithRSA(password, rsaPublicKey);
-      // const _user = await profilesRepository.update(user.id, { alcuin_password: encryptedPassword });
-      // if (_user) setUser(_user);
+      const encryptedPassword = encryptDataWithRSA(password, rsaPublicKey);
+      const _user = await profilesRepository.update(user.id, { alcuin_password: encryptedPassword });
+      if (_user) setUser(_user);
     } catch (err) {
       toastError(err);
     }
