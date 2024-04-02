2a7e1f06cf6047fcb5b46985ef2c40ebfd31992f
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-04-02 10:47:27 +0200

    :bug: Fixed an issue with the ADD_HOURS_OFFSET

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,5 +1,7 @@
+const parsedAddHoursOffset = parseInt(process.env.NEXT_PUBLIC_ADD_HOURS_OFFSET ?? '');
+
 const config = {
-  ADD_HOURS_OFFSET: parseInt(process.env.NEXT_PUBLIC_ADD_HOURS_OFFSET ?? '') ?? 0,
+  ADD_HOURS_OFFSET: isNaN(parsedAddHoursOffset) ? 0 : parsedAddHoursOffset,
 };
 
 export default config;
